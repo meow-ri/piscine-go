4466570a16020cd0b56783091b67b835fcd78256
meow-ri <umba.araika19@gmail.com>
2019-10-16 02:51:25 +0600

    printreversealphabet2

--- printreversealphabet/main.go
+++ printreversealphabet/main.go
@@ -3,8 +3,8 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-    for i := 'z'; i >= 'a'; i-- {
-        z01.PrintRune(i)
-    }
-    z01.PrintRune('\n')
+	for i := 'z'; i >= 'a'; i-- {
+		z01.PrintRune(i)
+	}
+	z01.PrintRune('\n')
 }
