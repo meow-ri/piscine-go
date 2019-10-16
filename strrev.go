888f7d2db2f8fc6907d27774163da06afdb4d471
meow-ri <umba.araika19@gmail.com>
2019-10-17 03:36:05 +0600

    strrev

---
+++ strrev.go
@@ -0,0 +1,17 @@
+package strlen
+
+func StrRev(str string) string {
+	runes := []rune(str)
+	var len int = 0
+	var rRune rune
+	for value := range runes {
+		len++
+		value = value
+	}
+	for i := 0; i < len/2; i++ {
+		rRune = runes[i]
+		runes[i] = runes[len-i-1]
+		runes[len-i-1] = rRune
+	}
+return string(runes)
+}
