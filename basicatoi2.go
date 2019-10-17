87ba1ee60a17f70d614cc96c9d72bb75dab87c33
meow-ri <umba.araika19@gmail.com>
2019-10-17 23:54:26 +0600

    basicatoi2

---
+++ basicatoi2.go
@@ -0,0 +1,33 @@
+package piscine
+
+func BasicAtoi2(s string) int {
+	runes := []rune(s)
+	var res int = 0
+	for _, r := range runes {
+		res *= 10
+		if r == '0' {
+			res += 0
+		} else if r == '1' {
+			res += 1
+		} else if r == '2' {
+			res += 2
+		} else if r == '3' {
+			res += 3
+		} else if r == '4' {
+			res += 4
+		} else if r == '5' {
+			res += 5
+		} else if r == '6' {
+			res += 6
+		} else if r == '7' {
+			res += 7
+		} else if r == '8' {
+			res += 8
+		} else if r == '9' {
+			res += 9
+		} else {
+			return 0
+		}
+	}
+	return res
+}
