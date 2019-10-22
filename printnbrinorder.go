524f078fc7cfac3c7d855d1c87ab8218e96a0fd8
meow-ri <umba.araika19@gmail.com>
2019-10-22 21:30:27 +0600

    printnb

---
+++ printnbrinorder.go
@@ -0,0 +1,17 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintNbrInOrder(n int) {
+	var nb []int
+	if n == 0 {
+		nb = append(nb, 0)
+	}
+	for ; n > 0; n /= 10 {
+		nb = append(nb, n%10)
+	}
+	SortIntegerTable(nb)
+	for i := range nb {
+		z01.PrintRune(nb[i] + '0')
+	}
+}
