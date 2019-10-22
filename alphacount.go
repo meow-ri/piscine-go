b4b8497bf139ea972925bf84c1e7054aa85e0379
meow-ri <umba.araika19@gmail.com>
2019-10-22 21:00:29 +0600

    alphacount

--- alphacount.go
+++ alphacount.go
@@ -1,11 +1,11 @@
 package piscine
 
 func AlphaCount(str string) int {
-
+	runes := []rune(str)
 	var n int = StrLen(str)
 	var res int = 0
 	for i := 0; i < n; i++ {
-		if (str[i] <= 'Z' && str >= 'A') || (str[i] <= 'z' && str >= 'a') {
+		if (rune[i] <= 'Z' && rune >= 'A') || (rune[i] <= 'z' && rune >= 'a') {
 			res++
 		}
 	}
