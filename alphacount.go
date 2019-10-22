ffbc020f55ec4049993fd3f5bae405d1ab4d456b
meow-ri <umba.araika19@gmail.com>
2019-10-22 21:02:32 +0600

    alphacount

--- alphacount.go
+++ alphacount.go
@@ -5,7 +5,7 @@ func AlphaCount(str string) int {
 	var n int = StrLen(str)
 	var res int = 0
 	for i := 0; i < n; i++ {
-		if (rune[i] <= 'Z' && rune >= 'A') || (rune[i] <= 'z' && rune >= 'a') {
+		if (runes[i] <= 'Z' && runes[i] >= 'A') || (runes[i] <= 'z' && runes[i] >= 'a') {
 			res++
 		}
 	}
